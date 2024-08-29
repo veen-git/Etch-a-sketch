5c2a4b633d31af50586d2d8785bf9f3779a36103
veen-git <sakhamuri.praveen@gmail.com>
2024-08-29 12:22:50 -0700

    Add event listeners to  buttons
    event listeners change colors of divs when mouse passes over a div. include seperate event listeners for each button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let d = 16;
 createDiv(d);
 
-const button = document.querySelector("button")
+const button = document.querySelector("#size")
 button.addEventListener("click", () => {
   let c = parseInt(prompt("pick a number less than 100"));
   (c > 100)?alert("number too high"): deleteDiv();
@@ -14,24 +14,36 @@ function createDiv(squares) {
     let i = 0; 
         while (i < (squares**2)){
             const div = document.createElement("div");
+              div.addEventListener("mouseover", () => {
+              div.style.background = "black"
+            })
+
+            document.querySelector("#eraser").addEventListener("click", () => {
+              div.addEventListener("mouseover", () => {
+                div.style.background = "white"
+            })
+          })
+
+            document.querySelector("#pen").addEventListener("click", () => {
+              div.addEventListener("mouseover", () => {
+                div.style.background = "black"
+              })
+        })
+
             div.setAttribute("class","paper")
             div.style.flexGrow = "1"
             div.style.flexShrink = "1"
             div.style.height = `${100/squares}%`
             div.style.width = `${100/squares}%`
            
-            
-        
             divContainer.appendChild(div);
         
             i++
         }   
-}
+      }
 
 function deleteDiv() {
    const parent = document.getElementById("Container")
    while (parent.firstChild) parent.removeChild(parent.firstChild)
 }
-
-
 
